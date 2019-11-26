151246c965a836fca3658c30dc8df3d9d03eec63
Priest4612 <prie4612@gmail.com>
2019-11-26 15:43:21 +0300

    Импрот модулей day-list.js, day.js. Изменил логику отрисовки списка дней и дня

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import {createTripInfoTemplate} from './components/trip-info.js';
 import {createSiteMenuTemplate} from './components/site-menu.js';
 import {createFilterTemplate} from './components/filter.js';
 import {createSortTemplate} from './components/sort.js';
+import {createDayListTemplate} from './components/day-list.js';
+import {createDayTemplate} from './components/day.js';
 import {createEventListTemplate} from './components/event-list.js';
 import {createEventEditTemplate} from './components/event-edit.js';
 import {createEventTemplate} from './components/event.js';
@@ -15,24 +17,27 @@ const render = (container, template, place) => {
 
 const sitePageBodyElement = document.querySelector(`.page-body`);
 const siteHeaderElement = sitePageBodyElement.querySelector(`.page-header`);
+
 const headerTripInfoElement = siteHeaderElement.querySelector(`.trip-info`);
 const headerTripControlsElement = siteHeaderElement.querySelector(`.trip-controls`);
 const headerHiddenElements = headerTripControlsElement.querySelectorAll(`.visually-hidden`);
-
 render(headerTripInfoElement, createTripInfoTemplate(), `afterbegin`);
 render(headerHiddenElements[0], createSiteMenuTemplate(), `afterend`);
 render(headerHiddenElements[1], createFilterTemplate(), `afterend`);
 
 const siteMainElement = sitePageBodyElement.querySelector(`.page-main`);
 const mainTripEventsElement = siteMainElement.querySelector(`.trip-events`);
-
 render(mainTripEventsElement, createSortTemplate(), `beforeend`);
-render(mainTripEventsElement, createEventListTemplate(), `beforeend`);
+render(mainTripEventsElement, createDayListTemplate(), `beforeend`);
 
-const eventListElement = mainTripEventsElement.querySelector(`.trip-days`);
+const dayListElement = mainTripEventsElement.querySelector(`.trip-days`);
+render(dayListElement, createDayTemplate(), `beforeend`);
 
+const dayElement = dayListElement.querySelector(`.day`);
+render(dayElement, createEventListTemplate(), `beforeend`);
+
+const eventListElement = dayElement.querySelector(`.trip-events__list`);
 render(eventListElement, createEventEditTemplate(), `beforeend`);
-
 new Array(EVENT_COUNT).fill().forEach(
     () => {
       render(eventListElement, createEventTemplate(), `beforeend`);
