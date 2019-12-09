364848ef81220c93aed33bda5df76016f4d0942a
Priest4612 <prie4612@gmail.com>
2019-12-09 16:11:41 +0300

    Создан список констант

---
+++ src/const.js
@@ -0,0 +1,35 @@
+export const TransferList = [
+  {title: `taxi`, checked: false},
+  {title: `bus`, checked: false},
+  {title: `train`, checked: false},
+  {title: `ship`, checked: false},
+  {title: `transport`, checked: false},
+  {title: `drive`, checked: false},
+  {title: `flight`, checked: false}
+];
+
+export const ActivityList = [
+  {title: `check-in`, checked: false},
+  {title: `restaurant`, checked: false},
+  {title: `sightseeing`, checked: false},
+];
+
+export const Cities = [
+  {location: `Saint Petersburg`, checked: true},
+  {location: `Amsterdam`, checked: false},
+  {location: `Geneva`, checked: false},
+  {location: `Chamonix`, checked: false},
+];
+
+export const OfferList = [
+  {title: `Add luggage`, price: `0`, type: `luggage`, checked: false},
+  {title: `Switch to comfort class`, price: `0`, type: `comfort`, checked: false},
+  {title: `Add meal`, price: 0, type: `meal`, checked: false},
+  {title: `Choose seats`, price: 0, type: `seats`, checked: false},
+  {title: `Travel by train`, price: 0, type: `train`, checked: false},
+  {title: `Rent a car`, price: 0, type: `car`, checked: false},
+  {title: `Add breakfast`, price: 0, type: `breakfast`, checked: false},
+  {title: `Book tickets`, price: 0, type: `tickets`, checked: false},
+  {title: `Lunch in city`, price: 0, type: `city`, checked: false},
+  {title: `Order Uber`, price: 0, type: `uber`, checked: false},
+];
