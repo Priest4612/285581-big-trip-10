b5f9a3b332930091d38f828ceadb7053e06184aa
Priest4612 <prie4612@gmail.com>
2019-12-10 15:47:33 +0300

    добавил подстановку данных в шаблон

--- src/components/filter.js
+++ src/components/filter.js
@@ -1,20 +1,30 @@
+import {FilterList} from '../const.js';
+
+const createFilterItem = (filterItem) => {
+  return (
+    `<div class="trip-filters__filter">
+      <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything"
+      ${filterItem.checked ? `checked` : ``}>
+      <label class="trip-filters__filter-label" for="filter-everything">${filterItem.title}</label>
+    </div>`
+  );
+};
+
+
+const filterListMurkup = (filterList) => {
+  return Array
+  .from(filterList)
+  .map((filterItem) => {
+    return createFilterItem(filterItem);
+  }).join(`\n`);
+};
+
+
 export const createFilterTemplate = () => {
   return (
     `<form class="trip-filters" action="#" method="get">
-      <div class="trip-filters__filter">
-        <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
-        <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
-      </div>
 
-      <div class="trip-filters__filter">
-        <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
-        <label class="trip-filters__filter-label" for="filter-future">Future</label>
-      </div>
-
-      <div class="trip-filters__filter">
-        <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
-        <label class="trip-filters__filter-label" for="filter-past">Past</label>
-      </div>
+      ${filterListMurkup(FilterList)}
 
       <button class="visually-hidden" type="submit">Accept filter</button>
     </form>`
