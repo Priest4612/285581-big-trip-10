38954e44898e97121d530a6d2caf42c00e6a3307
priest4612 <prie4612@gmail.com>
2019-12-12 00:30:48 +0300

    Обновил функцию копирования массива

--- src/utils.js
+++ src/utils.js
@@ -45,13 +45,28 @@ const isObject = (object) => {
   return type === `function` || type === `object`;
 };
 
+const cloneObject = (source) => {
+  const clone = {};
+
+  for (const key in source) {
+    if (source.hasOwnProperty(key)) {
+      if (isObject(source[key])) {
+        clone[key] = cloneObject(source[key]);
+      } else {
+        clone[key] = source[key];
+      }
+    }
+  }
+  return clone;
+};
+
 export const cloneArray = (source) => {
   const clone = [];
 
   for (const key in source) {
     if (source.hasOwnProperty(key)) {
       if (isObject(source[key])) {
-        clone[key] = cloneArray(source[key]);
+        clone[key] = cloneObject(source[key]);
       } else {
         clone[key] = source[key];
       }
