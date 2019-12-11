e6c50ec0ae0ffbbf7c10cb98cfc38d42acce956f
priest4612 <prie4612@gmail.com>
2019-12-12 00:27:40 +0300

    Обновил исходные данные в соответствии с ТЗ

--- src/const.js
+++ src/const.js
@@ -1,37 +1,37 @@
 export const TransferList = [
-  {title: `taxi`, checked: false, price: 0},
-  {title: `bus`, checked: false, price: 0},
-  {title: `train`, checked: false, price: 0},
-  {title: `ship`, checked: false, price: 0},
-  {title: `transport`, checked: false, price: 0},
-  {title: `drive`, checked: false, price: 0},
-  {title: `flight`, checked: false, price: 0},
+  {title: `taxi`, price: 0, type: `taxi`},
+  {title: `bus`, price: 0, type: `bus`},
+  {title: `train`, price: 0, type: `train`},
+  {title: `ship`, price: 0, type: `ship`},
+  {title: `transport`, price: 0, type: `transport`},
+  {title: `drive`, price: 0, type: `drive`},
+  {title: `flight`, price: 0, type: `flight`},
 ];
 
 export const ActivityList = [
-  {title: `check-in`, checked: false, price: 0},
-  {title: `restaurant`, checked: false, price: 0},
-  {title: `sightseeing`, checked: false, price: 0},
+  {title: `check-in`, price: 0, type: `check-in`},
+  {title: `restaurant`, price: 0, type: `restaurant`},
+  {title: `sightseeing`, price: 0, type: `sightseeing`},
 ];
 
 export const Cities = [
-  {title: `Saint Petersburg`, checked: false},
-  {title: `Amsterdam`, checked: false},
-  {title: `Geneva`, checked: false},
-  {title: `Chamonix`, checked: false},
+  {title: `Saint Petersburg`},
+  {title: `Amsterdam`},
+  {title: `Geneva`},
+  {title: `Chamonix`},
 ];
 
 export const OfferList = [
-  {title: `Add luggage`, price: `0`, type: `luggage`, checked: false},
-  {title: `Switch to comfort class`, price: `0`, type: `comfort`, checked: false},
-  {title: `Add meal`, price: 0, type: `meal`, checked: false},
-  {title: `Choose seats`, price: 0, type: `seats`, checked: false},
-  {title: `Travel by train`, price: 0, type: `train`, checked: false},
-  {title: `Rent a car`, price: 0, type: `car`, checked: false},
-  {title: `Add breakfast`, price: 0, type: `breakfast`, checked: false},
-  {title: `Book tickets`, price: 0, type: `tickets`, checked: false},
-  {title: `Lunch in city`, price: 0, type: `city`, checked: false},
-  {title: `Order Uber`, price: 0, type: `uber`, checked: false},
+  {title: `Add luggage`, price: `0`, type: `luggage`},
+  {title: `Switch to comfort class`, price: `0`, type: `comfort`},
+  {title: `Add meal`, price: 0, type: `meal`},
+  {title: `Choose seats`, price: 0, type: `seats`},
+  {title: `Travel by train`, price: 0, type: `train`},
+  {title: `Rent a car`, price: 0, type: `car`},
+  {title: `Add breakfast`, price: 0, type: `breakfast`},
+  {title: `Book tickets`, price: 0, type: `tickets`},
+  {title: `Lunch in city`, price: 0, type: `city`},
+  {title: `Order Uber`, price: 0, type: `uber`},
 ];
 
 export const MenuList = [
