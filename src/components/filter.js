799b94a88cd0bd08f447684517781666c3c9becf
priest4612 <prie4612@gmail.com>
2019-12-12 22:30:19 +0300

    Добавил водстановку данных для фильтров

--- src/components/filter.js
+++ src/components/filter.js
@@ -3,7 +3,12 @@ import {FilterList} from '../const.js';
 const createFilterItem = (filterItem) => {
   return (
     `<div class="trip-filters__filter">
-      <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything"
+      <input
+      id="filter-${filterItem.title}"
+      class="trip-filters__filter-input  visually-hidden"
+      type="radio"
+      name="trip-filter"
+      value="-${filterItem.title}"
       ${filterItem.checked ? `checked` : ``}>
       <label class="trip-filters__filter-label" for="filter-everything">${filterItem.title}</label>
     </div>`
