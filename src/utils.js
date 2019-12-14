ef7bb5985809c62a86d3cda6d15b897ec8380f90
Priest4612 <prie4612@gmail.com>
2019-12-14 20:54:03 +0300

    Создал функции: создания и отрисовки элементов разметки

--- src/utils.js
+++ src/utils.js
@@ -74,3 +74,32 @@ export const cloneArray = (source) => {
   }
   return clone;
 };
+
+
+export const createElement = (template) => {
+  const newElement = document.createElement(`div`);
+  newElement.innerHTML = template;
+
+  return newElement.firstElementChild;
+};
+
+
+export const RenderPosition = {
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`,
+  AFTEREND: `afterend`
+};
+
+export const render = (container, element, place) => {
+  switch (place) {
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+    case RenderPosition.AFTEREND:
+      container.after(element);
+      break;
+  }
+};
