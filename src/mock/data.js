88f22142bc487ca65b352ea35d54cd8e837efe72
priest4612 <prie4612@gmail.com>
2019-12-16 13:46:06 +0300

    Заменил логику генерации данных

--- src/mock/data.js
+++ src/mock/data.js
@@ -1,7 +1,9 @@
 import {generateEvents} from './event-mock.js';
 
-const EVENT_COUNT = Math.random() > 0.2 ? 0 : 20;
+const EVENT_COUNT = Math.random() > 0.9 ? 0 : 20;
 
 const data = generateEvents(EVENT_COUNT);
+
+// data.forEach((item) => console.log(`start: ${item.dateStart}, end ${item.dateEnd}`));
 
 export {data};
