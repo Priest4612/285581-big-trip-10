8f07dda4b50cf9f0ad7eb5452730727258f28022
priest4612 <prie4612@gmail.com>
2019-12-16 13:37:50 +0300

    Добавил улучшенный генератор даты

--- src/utils.js
+++ src/utils.js
@@ -18,6 +18,32 @@ export const getRandomDate = (endEvent = 30) => {
   return targetDate;
 };
 
+const getRandomHours = (date, intervalHours) => {
+  const targetDate = new Date(date);
+  const hours = getRandomInteger(targetDate.getHours(), intervalHours) % 24;
+  const minutes = getRandomInteger(targetDate.getMinutes(), 59);
+  targetDate.setHours(targetDate.getHours() + hours, targetDate.getMinutes() + minutes);
+  return targetDate;
+};
+
+export const getRandomDateDay = (referencePoint = null, startDayEvent = 3, intervalHours = 8) => {
+  let dateStart = 0;
+  let dateEnd = 0;
+
+  if (!referencePoint) {
+    const targetDate = new Date();
+    targetDate.setDate(targetDate.getDate() - getRandomInteger(0, startDayEvent));
+    dateStart = getRandomHours(targetDate, intervalHours);
+    dateEnd = getRandomHours(dateStart, intervalHours);
+  } else {
+    const targetDate = referencePoint;
+    dateStart = getRandomHours(targetDate, intervalHours);
+    dateEnd = getRandomHours(dateStart, intervalHours);
+  }
+
+  return {dateStart, dateEnd};
+};
+
 export const castTimeFormat = (value) => {
   return value < 10 ? `0${value}` : value;
 };
