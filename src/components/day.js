c4affa1a9006efeee4ea45bf48fe4c66d304e2af
priest4612 <prie4612@gmail.com>
2019-12-16 16:26:50 +0300

    Добавил подстановку даты

--- src/components/day.js
+++ src/components/day.js
@@ -1,30 +1,34 @@
-import {createElement} from "../utils";
+import {createElement, formatDateTime} from "../utils";
+import {MonstList} from "../const";
 
-const createDateMarkup = () => {
+const createDateMarkup = (date) => {
   return (
     `<div class="day__info">
-      <span class="day__counter">1</span>
-      <time class="day__date" datetime="2019-03-18">MAR 18</time>
+      <span class="day__counter">${date.getDate()}</span>
+      <time class="day__date" datetime="${formatDateTime(date).date}">
+        ${MonstList[date.getMonth()]} ${String(date.getFullYear()).slice(2)}
+      </time>
     </div>`
   );
 };
 
-const createDayTemplate = () => {
+const createDayTemplate = (date) => {
   return (
     `<li class="trip-days__item  day">
-      ${createDateMarkup()}
+      ${createDateMarkup(date)}
     </li>`
   );
 };
 
 
 export default class DayItem {
-  constructor() {
+  constructor(date) {
+    this._date = date;
     this._element = null;
   }
 
   getTemplate() {
-    return createDayTemplate();
+    return createDayTemplate(this._date);
   }
 
   getElement() {
