1a9b9711f47e9e07847b6b1928f978cc25a4b446
Priest4612 <prie4612@gmail.com>
2020-01-16 15:23:56 +0300

    Создал класс PointController для отрисовки точек маршрута

---
+++ src/controllers/point-controller.js
@@ -0,0 +1,77 @@
+import PointComponent from '../components/point.js';
+import PointEditComponent from '../components/point-edit.js';
+import {replace, render, RenderPosition} from '../utils/render.js';
+
+
+const Mode = {
+  DEFAULT: `default`,
+  EDIT: `edit`,
+};
+
+
+export default class PointController {
+  constructor(container) {
+    this._container = container;
+
+    this._mode = Mode.DEFAULT;
+
+    this._pointComponent = null;
+    this._pointEditComponent = null;
+
+    this._onEscKeyDown = this._onEscKeyDown.bind(this);
+    this._onCloseForm = this._onCloseForm.bind(this);
+  }
+
+  render(point) {
+    const oldPointComponent = this._pointComponent;
+    const oldPointEditComponent = this._pointEditComponent;
+
+    this._pointComponent = new PointComponent(point);
+    this._pointEditComponent = new PointEditComponent(point);
+
+    this._pointComponent.setOpenEditButtonClickHandler(() => {
+      this._replacePointToEdit();
+      document.addEventListener(`keydown`, this._onEscKeyDown);
+    });
+
+    this._pointEditComponent.setSubmitHandler(this._onCloseForm);
+    this._pointEditComponent.setCloseEditButtonClickHandler(this._onCloseForm);
+
+    if (oldPointEditComponent && oldPointComponent) {
+      replace(this._pointComponent, oldPointComponent);
+      replace(this._pointEditComponent, oldPointEditComponent);
+    } else {
+      render(this._container, this._pointComponent, RenderPosition.BEFOREEND);
+    }
+  }
+
+  setDefaultView() {
+    if (this._mode !== Mode.Default) {
+      this._replaceEditToPoint();
+    }
+  }
+
+  _replaceEditToPoint() {
+    replace(this._pointComponent, this._pointEditComponent);
+    this._mode = Mode.DEFAULT;
+  }
+
+  _replacePointToEdit() {
+    replace(this._pointEditComponent, this._pointComponent);
+    this._mode = Mode.EDIT;
+  }
+
+  _onEscKeyDown(evt) {
+    const isEscKey = evt.key === `Escape` || evt.kay === `Esc`;
+
+    if (isEscKey) {
+      this._replaceEditToPoint();
+      document.removeEventListener(`keydown`, this._onEscKeyDown);
+    }
+  }
+
+  _onCloseForm() {
+    this._replaceEditToPoint();
+    document.removeEventListener(`keydown`, this._onEscKeyDown);
+  }
+}
